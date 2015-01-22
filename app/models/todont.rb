e77329dd0880f5dc30c08db2845e82e4e4b0328d
Liam Sean Brady <liamseanbrady@gmail.com>
2015-01-22 06:18:58 +0000

    Add has_many association for success_days.

--- app/models/todont.rb
+++ app/models/todont.rb
@@ -1,6 +1,7 @@
 class Todont < ActiveRecord::Base
   belongs_to :creator, foreign_key: 'user_id', class_name: 'User'
   has_many :progress_logs
+  has_many :success_days
 
   validates :body, length: {minimum: 3, maximum: 60}
   validates :visibility, inclusion: { in: %w(private, public) }
