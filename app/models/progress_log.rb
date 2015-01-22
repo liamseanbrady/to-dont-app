e1301dccde4d7ec97f967f0768ec04639558858d
Liam Sean Brady <liamseanbrady@gmail.com>
2015-01-22 05:46:14 +0000

    Add belongs_to association for todont. Add validations for message and mood.

---
+++ app/models/progress_log.rb
@@ -0,0 +1,6 @@
+class ProgressLog < ActiveRecord::Base
+  belongs_to :todont
+
+  validates :message, length: { minimum: 6, maximum: 100 }
+  validates :mood, inclusion: { in %w(motivated, sad, zapped, happy, excited) }
+end
