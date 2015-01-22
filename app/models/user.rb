09dea9cf15340bce8bae30ad3b7cd3c3dbe8cb60
Liam Sean Brady <liamseanbrady@gmail.com>
2015-01-22 06:07:18 +0000

    Add has_many through association on todonts in order to retrieve all of the user's progress_logs.

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,7 @@ class User < ActiveRecord::Base
   has_secure_password validations: false
 
   has_many :todonts
+  has_many :progress_logs, through: :todonts, source: :progress_logs
 
   validates :username, length: {minumum: 3, maximum: 20}
   validates :password, length: {minimum: 8, maximum: 30}
