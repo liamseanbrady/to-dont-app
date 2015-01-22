363c234fe7381ab8fd25980826fee60c482f9d3c
Liam Sean Brady <liamseanbrady@gmail.com>
2015-01-22 05:32:03 +0000

    Add validations on body and visibility.

--- app/models/todont.rb
+++ app/models/todont.rb
@@ -1,3 +1,6 @@
 class Todont < ActiveRecord::Base
   belongs_to :creator, foreign_key: 'user_id', class_name: 'User'
+
+  validates :body, length: {minimum: 3, maximum: 60}
+  validates :visibility, inclusion: { in %w(private, public) }
 end
