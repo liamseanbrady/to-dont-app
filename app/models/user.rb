a2aac5d1b9c0bac7c147d804241c92253c62e394
Liam Sean Brady <liamseanbrady@gmail.com>
2015-01-22 05:31:43 +0000

    Add validations on username and password.

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,7 @@ class User < ActiveRecord::Base
   has_secure_password validations: false
 
   has_many :todonts
+
+  validates :username, length: {minumum: 3, maximum: 20}
+  validates :password, length: {minimum: 8, maximum: 30}
 end
