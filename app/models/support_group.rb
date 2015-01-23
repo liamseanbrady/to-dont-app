f4427acb0a858c4a1ebf227519d3ad3b6610d5d6
Liam Sean Brady <liamseanbrady@gmail.com>
2015-01-23 02:38:09 +0000

    Add has_many memberships and has_many participants through memberships.

--- app/models/support_group.rb
+++ app/models/support_group.rb
@@ -1,6 +1,6 @@
 class SupportGroup < ActiveRecord::Base
-  has_many :memberships
-  has_many :participants, through: :memberships, source_type: 'SupportGroup', source: :user
+  has_many :memberships, as: :joinable
+  has_many :participants, through: :memberships, source: :user
 
   validates :name, length: { minimum: 5, maximum: 20 }, uniqueness: true
   validates :moderator, presence: true
