00ecd672b530a6766f0d27e98c29619c44e411fb
Liam Sean Brady <liamseanbrady@gmail.com>
2015-01-23 02:49:39 +0000

    Add has_many through assoication for support group memberships. Add has_many association for support groups through support group memberships.

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,8 @@ class User < ActiveRecord::Base
 
   has_many :todonts
   has_many :progress_logs, through: :todonts, source: :progress_logs
+  has_many :support_group_memberships, class_name: 'Membership'
+  has_many :support_groups, through: :support_group_memberships, source: :joinable, source_type: 'SupportGroup'
 
   validates :username, length: {minumum: 3, maximum: 20}, uniqueness: true
   validates :password, length: {minimum: 8, maximum: 30}
