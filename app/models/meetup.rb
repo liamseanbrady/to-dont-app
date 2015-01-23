56bbdeeabfec4315198368ac3374c331e9672028
Liam Sean Brady <liamseanbrady@gmail.com>
2015-01-23 03:57:29 +0000

    Add belongs_to for organizer. Add has_many through for attendee memberships and has_many attendees. Add validations.

---
+++ app/models/meetup.rb
@@ -0,0 +1,12 @@
+class Meetup < ActiveRecord::Base
+  belongs_to :organizer, class_name: 'User'
+
+  has_many :attendee_memberships, as: :joinable, class_name: "Membership"
+  has_many :attendees, through: :attendee_memberships, source: :user
+
+  validates :title, length: { minimum: 5, maximum: 20 }
+  validates :description, length: { minimum: 10, maximum: 200 }
+  validates :duration, inclusion: { in: [1, 2, 3, 4, 5, 6, 7] }
+  validates :location, length: { minimum: 3, maximum: 60 }
+  validates :event_date, presence: true
+end
