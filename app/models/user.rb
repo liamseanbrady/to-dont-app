23272e3f75324099b0b08afd0ef949e7fd828625
Liam Sean Brady <liamseanbrady@gmail.com>
2015-01-23 03:59:21 +0000

    Add has_many through for meetup memberships and has_many for meetups scheduled. Add has_many for meetups_organized.

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,6 +7,11 @@ class User < ActiveRecord::Base
   has_many :support_group_memberships, class_name: 'Membership'
   has_many :support_groups, through: :support_group_memberships, source: :joinable, source_type: 'SupportGroup'
 
+  has_many :meetup_memberships, class_name: "Membership"
+  has_many :meetups_scheduled, through: :meetup_memberships, source: :joinable, source_type: 'Meetup'
+
+  has_many :organized_meetups, foreign_key: 'organizer_id', class_name: 'Meetup'
+
   validates :username, length: {minumum: 3, maximum: 20}, uniqueness: true
   validates :password, length: {minimum: 8, maximum: 30}
 end
