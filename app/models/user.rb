af02eb6827b2445fc56e45bcb73d0f1296f8f0ef
Liam Sean Brady <liamseanbrady@gmail.com>
2015-01-23 03:02:02 +0000

    Minor formatting change.

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,7 @@ class User < ActiveRecord::Base
 
   has_many :todonts
   has_many :progress_logs, through: :todonts, source: :progress_logs
+
   has_many :support_group_memberships, class_name: 'Membership'
   has_many :support_groups, through: :support_group_memberships, source: :joinable, source_type: 'SupportGroup'
 
