15a109e49ef8f7a0fceeb8942a97c7b97417f9b2
Liam Sean Brady <liamseanbrady@gmail.com>
2015-01-23 08:32:14 +0000

    Add on: :create to password validation. Add methods to generate and set it to the slug column before saving..

--- app/models/user.rb
+++ app/models/user.rb
@@ -13,5 +13,39 @@ class User < ActiveRecord::Base
   has_many :organized_meetups, foreign_key: 'organizer_id', class_name: 'Meetup'
 
   validates :username, length: {minumum: 3, maximum: 20}, uniqueness: true
-  validates :password, length: {minimum: 8, maximum: 30}
+  validates :password, length: {minimum: 8, maximum: 30}, on: :create
+
+  before_save :generate_slug!
+
+  def to_param
+    sanitize_for_url
+  end
+
+  def generate_slug!
+    uniq_slug = sanitize_for_url
+    user = User.find_by(slug: uniq_slug)
+    uniq_id = 2
+
+    while user && user != self
+      uniq_slug = append_suffix(uniq_slug, uniq_id)
+      user = User.find_by(slug: uniq_slug)
+      uniq_id += 1
+    end
+
+    self.slug = uniq_slug
+  end
+
+  def append_suffix(slug, id)
+    if slug.split('-').last.to_i != 0
+      slug = slug.slice(0...-1)
+    end
+
+    "#{slug}-#{id}"
+  end
+
+  def sanitize_for_url
+    str = self.username
+    str.strip.gsub(/\s+/, '-').gsub(/-+/, '-').gsub(/-+\z/, '').gsub(/\A-+/, '')
+  end
 end
+
