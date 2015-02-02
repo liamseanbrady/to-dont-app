b06c1a22d0a4a237ab59e4c7d3479363edb2b064
Liam Sean Brady <liamseanbrady@gmail.com>
2015-02-02 14:47:44 +0000

    Add users/new route back in. Add explicit named routes where appropriate.

--- config/routes.rb
+++ config/routes.rb
@@ -3,9 +3,12 @@ ToDontApp::Application.routes.draw do
 
   get 'register', to: 'users#new', as: 'register'
 
-  get '/users', to: 'users#index'
+  # How to does Rails cancel out users/new so users/:id doesn't pick it up? Figure and include in README
+
+  get '/users', to: 'users#index', as: 'users'
   post '/users', to: 'users#create'
-  get '/users/:id/edit', to: 'users#edit'
+  get '/users/new', to: 'users#new', as: 'new_user'
+  get '/users/:id/edit', to: 'users#edit', as: 'edit_user'
   get '/users/:id', to: 'users#show', as: 'user'
   put '/users/:id', to: 'users#update'
   patch '/users/:id', to: 'users#update'
