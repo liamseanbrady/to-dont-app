4dcb957d3f63ee150c2ad78d006443f1f267d60d
Liam Sean Brady <liamseanbrady@gmail.com>
2015-02-02 13:28:13 +0000

    Remove get to users/new route and replace with get to /register route.

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 ToDontApp::Application.routes.draw do
   root 'users#index'
 
+  get 'register', to: 'users#new', as: 'register'
+
   get '/users', to: 'users#index'
   post '/users', to: 'users#create'
-  get '/users/new', to: 'users#new'
   get '/users/:id/edit', to: 'users#edit'
   get '/users/:id', to: 'users#show', as: 'user'
   put '/users/:id', to: 'users#update'
