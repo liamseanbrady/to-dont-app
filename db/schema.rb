d2672f925f9f3a8f4ae46d2d099f35c1e5671395
Liam Sean Brady <liamseanbrady@gmail.com>
2015-02-02 12:21:04 +0000

    Add email column to users table.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150123065930) do
+ActiveRecord::Schema.define(version: 20150202120920) do
 
   create_table "meetups", force: true do |t|
     t.string   "title"
@@ -71,6 +71,7 @@ ActiveRecord::Schema.define(version: 20150123065930) do
     t.datetime "updated_at"
     t.string   "role"
     t.string   "slug"
+    t.string   "email"
   end
 
 end
