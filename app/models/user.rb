80bbe8b15b68af2707523262cc7e8fba5891ded8
Liam Sean Brady <liamseanbrady@gmail.com>
2015-02-02 13:17:02 +0000

    Include sluggable module and add sluggable_column as username attribute.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ActiveRecord::Base
   include Gravatarable
+  include Sluggable
 
   has_secure_password validations: false
 
@@ -19,38 +20,8 @@ class User < ActiveRecord::Base
   validates :password, length: {minimum: 8, maximum: 30}, on: :create
 
   gravatar_column :email
+  sluggable_column :username
 
   before_save :generate_slug!
-
-  def to_param
-    sanitize_for_url
-  end
-
-  def generate_slug!
-    uniq_slug = sanitize_for_url
-    user = User.find_by(slug: uniq_slug)
-    uniq_id = 2
-
-    while user && user != self
-      uniq_slug = append_suffix(uniq_slug, uniq_id)
-      user = User.find_by(slug: uniq_slug)
-      uniq_id += 1
-    end
-
-    self.slug = uniq_slug
-  end
-
-  def append_suffix(slug, id)
-    if slug.split('-').last.to_i != 0
-      slug = slug.slice(0...-1)
-    end
-
-    "#{slug}-#{id}"
-  end
-
-  def sanitize_for_url
-    str = self.username
-    str.strip.gsub(/\s+/, '-').gsub(/-+/, '-').gsub(/-+\z/, '').gsub(/\A-+/, '')
-  end
 end
 
