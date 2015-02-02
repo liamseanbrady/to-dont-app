b0fbe764432a85206df0f426420db1d31dda550e
Liam Sean Brady <liamseanbrady@gmail.com>
2015-02-02 13:16:33 +0000

    Include sluggable module and add sluggable_column as body attribute.

--- app/models/todont.rb
+++ app/models/todont.rb
@@ -1,8 +1,12 @@
 class Todont < ActiveRecord::Base
+  include Sluggable
+
   belongs_to :creator, foreign_key: 'user_id', class_name: 'User'
   has_many :progress_logs
   has_many :success_days
 
   validates :body, length: {minimum: 3, maximum: 60}
   validates :visibility, inclusion: { in: %w(private public) }
+
+  slugabble_column :body
 end
