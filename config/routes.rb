71d08050c044cccd127d33ed810a4831f77ed8df
Liam Sean Brady <liamseanbrady@gmail.com>
2015-02-02 14:57:42 +0000

    Add routes for logging in and logging out with sessions.

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 ToDontApp::Application.routes.draw do
   root 'users#index'
 
-  get 'register', to: 'users#new', as: 'register'
+  get '/register', to: 'users#new', as: 'register'
+  get '/login', to: 'sessions#new', as: 'login'
+  post '/login', to: 'session#create'
+  get '/logout', to: 'sessions#logout', as: 'logout'
 
   # How to does Rails cancel out users/new so users/:id doesn't pick it up? Figure and include in README
 
