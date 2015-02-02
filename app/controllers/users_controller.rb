ccf1a359671183a785133b2825103992a717e97e
Liam Sean Brady <liamseanbrady@gmail.com>
2015-02-02 14:45:57 +0000

    Add new and create actions.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,32 @@
 class UsersController < ApplicationController
   def index
+    binding.pry
     @users = User.all
+  end
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+
+    if @user.save
+      flash[:notice] = 'You successfully registered!'
+      redirect_to users_path
+      binding.pry
+    else
+      render :new
+    end
   end
 
   def show
     @user = User.find_by(slug: params[:id])
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:username, :email, :password, :password_confirm)
   end
 end
