e9f8d5cd1b5ca8f3dde2e3b63c3bc748683fb9c8
Liam Sean Brady <liamseanbrady@gmail.com>
2015-02-02 13:15:41 +0000

    Include sluggable module. Add sluggable_column as name attribute.

--- app/models/support_group.rb
+++ app/models/support_group.rb
@@ -1,7 +1,11 @@
 class SupportGroup < ActiveRecord::Base
+  include Sluggable
+
   has_many :memberships, as: :joinable
   has_many :participants, through: :memberships, source: :user
 
   validates :name, length: { minimum: 5, maximum: 20 }, uniqueness: true
   validates :moderator, presence: true
+
+  sluggable_column :name
 end
