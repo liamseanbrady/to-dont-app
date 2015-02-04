98914b39586da6756e6f86565828edd38f15b900
Liam Sean Brady <liamseanbrady@gmail.com>
2015-02-04 13:53:51 +0000

    Add progress log action to create new progress log.

--- app/controllers/todonts_controller.rb
+++ app/controllers/todonts_controller.rb
@@ -18,11 +18,29 @@ class TodontsController < ApplicationController
 
   def show
     @todont = Todont.find_by(slug: params[:id])
+    @progress_log = ProgressLog.new
+    @progress_logs = current_user.progress_logs
+  end
+
+  def progress_log
+    @progress_log = ProgressLog.new(progress_log_params)
+    @progress_log.todont = Todont.find_by(slug: params[:id])
+
+    if @progress_log.save
+      flash[:notice] = 'Your progress log was saved successfully.'
+      redirect_to user_todont_path(current_user, @progress_log.todont)
+    else
+      render :show
+    end
   end
 
   private
 
   def todont_params
     params.require(:todont).permit(:body)
+  end
+
+  def progress_log_params
+    params.require(:progress_log).permit(:message, :mood)
   end
 end
