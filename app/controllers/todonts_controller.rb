7853ca75b7a4b1fc49da366d2fb4c54bee087431
Liam Sean Brady <liamseanbrady@gmail.com>
2015-02-04 13:02:09 +0000

    In show action find todont by slug.

--- app/controllers/todonts_controller.rb
+++ app/controllers/todonts_controller.rb
@@ -1,9 +1,28 @@
 class TodontsController < ApplicationController
   def index
     @todonts = current_user.todonts
+    @todont = Todont.new
+  end
+
+  def create
+    @todont = Todont.new(todont_params)
+    @todont.creator = current_user
+
+    if @todont.save
+      flash[:notice] = "Your todon't was created successfully!"
+      redirect_to user_todonts_path(current_user)
+    else
+      render :index
+    end
   end
 
   def show
-    @todont = Todont.find(params[:id])
+    @todont = Todont.find_by(slug: params[:id])
+  end
+
+  private
+
+  def todont_params
+    params.require(:todont).permit(:body)
   end
 end
