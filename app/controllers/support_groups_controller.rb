08c5babf7ae2a90b63724bcc68dc60ca6316a5fa
Liam Sean Brady <liamseanbrady@gmail.com>
2015-02-14 12:27:57 +0000

    Use require_user before action. Add create action.

--- app/controllers/support_groups_controller.rb
+++ app/controllers/support_groups_controller.rb
@@ -1,5 +1,30 @@
 class SupportGroupsController < ApplicationController
+  before_action :require_user
+
   def index
-   @support_groups = SupportGroup.all 
+    @support_groups = SupportGroup.all 
+  end
+  
+  def new
+    @support_group = SupportGroup.new 
+  end
+
+  def create
+    @support_group = SupportGroup.new(support_group_params)
+    @support_group.moderator = current_user
+
+    if @support_group.save
+      flash[:notice] = "You created a new support group"
+      redirect_to user_support_groups_path(current_user)
+    else
+      flash[:error] = "The support group could not be created"
+      render :new
+    end
+  end
+
+  private
+
+  def support_group_params
+    params.require(:support_group).permit(:name)
   end
 end
