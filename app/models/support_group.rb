9017c3c7295190153eb34dfa666065b98931fd6e
Liam Sean Brady <liamseanbrady@gmail.com>
2015-02-14 12:30:35 +0000

    Add belongs_to association for moderator.

--- app/models/support_group.rb
+++ app/models/support_group.rb
@@ -3,6 +3,7 @@ class SupportGroup < ActiveRecord::Base
 
   has_many :memberships, as: :joinable
   has_many :participants, through: :memberships, source: :user
+  belongs_to :moderator, class_name: "User"
 
   validates :name, length: { minimum: 5, maximum: 20 }, uniqueness: true
   validates :moderator, presence: true
